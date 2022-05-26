5acc5c84fc391f2b55b3ee267483b0f9799c1053
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-05-26 16:14:56 +0900

    first commit

---
+++ src/GetReportResults.java
@@ -0,0 +1,8 @@
+public class GetReportResults {
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
+    }
+    public static void main(String[] args) {
+    }
+}
