dc4c5a7b0f23f2f890591cc9810c94e68136257f
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-06-10 17:51:10 +0900

    commit

---
+++ src/innerEnemy.java
@@ -0,0 +1,22 @@
+public class innerEnemy {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        int res = 0;
+        for (int i = 0; i < a.length; i++) {
+           res = a[i] * b[i];
+           answer += res;    
+        }
+
+        System.out.println(answer);
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        innerEnemy inn = new innerEnemy();
+        
+        int [] a = {1,2,3,4};
+        int [] b = {-3,-1,0,2};
+        
+        inn.solution(a, b);
+    }
+}
