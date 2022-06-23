ab47d9e5cb4e010a0ad481d5d17db2318e8db602
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-06-23 10:48:40 +0900

    commit

---
+++ src/LEVEL2/RowColumnRotation.java
@@ -0,0 +1,16 @@
+package LEVEL2;
+
+/**
+ * RowColumnRotation
+ */
+public class RowColumnRotation {
+
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
